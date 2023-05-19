b84a53e0dcdbecb94e9dab667c5d2b2cd6f2cef9
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-05-18 22:19:39 -0400

    remove extra bcrypt

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { User } = require("../../models");
-const bcrypt = require('bcrypt');
 
 // POST route that takes in data to create a new user
 router.post('/signup', async (req, res) => {
